8ef8aaceeeee64f09f59bad0ac599d68597dc05e
TeresaVu <teresavu353@gmail.com>
2023-06-12 10:13:32 -0400

    try/catch if no args for dir to watch

--- watchfiles.py
+++ watchfiles.py
@@ -59,11 +59,15 @@ def process_file(directory, file):
 
 
 def main():
-    # Enter the path to the desired directory to watch relative to the script's directory
-    # The directory of the script being run is os.path.dirname(os.path.abspath(__file__))
-    # directory_to_watch = os.path.dirname(os.path.abspath(__file__)) + "/PRE-STAGE"
-    directory_to_watch = os.path.dirname(os.path.abspath(__file__)) + "/" + sys.argv[1]
-    print(directory_to_watch)
+    try:
+        # Enter the path to the desired directory to watch relative to the script's directory
+        # The directory of the script being run is os.path.dirname(os.path.abspath(__file__))
+        directory_to_watch = os.path.dirname(os.path.abspath(__file__)) + "/" + sys.argv[1]
+    except:
+        # If no argument passed
+        directory_to_watch = os.path.dirname(os.path.abspath(__file__))
+
+    print(f"Watching {directory_to_watch}")
 
     process = subprocess.Popen(['inotifywait', '-q', '-m', '-r', '-e', 'modify,delete,create,moved_to', directory_to_watch],
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
